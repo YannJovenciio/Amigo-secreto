e8bc1678efc36fa7ded0ea9569e08f82c5f2c287
Yan <Yanjovenciomiranda@hotmail.com>
2025-01-20 21:17:19 -0300

    ´Primeiras funcionalidades

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigo = []
+function ExibirTexto(tag,texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
 
 
 function adicionarAmigo() {
 
+
+    let campoNome = document.getElementById('amigo').value;
+    nome = campoNome
+
+    if(nome == ""){
+        alert("Insira um nome")
+    }
+
+    if (nome) {
+        listaAmigo.push(nome);
+
+        console.log(listaAmigo)
+    }
+
+
+        
+    console.log("Nome do amigo: " + nome);
+    
+
+    LimparCampo();
+    mostrarLista();
 }
 
+
+
+function mostrarLista(){
+    ExibirTexto('ul',listaAmigo);
+    for (let i = 0; i < listaAmigo.length; i++) {
+        console.log(listaAmigo[i]);
+    }
+
+}
+function LimparCampo() {
+    nome = document.getElementById('amigo');
+    nome.value = '';
+}
+
+
+
+
+
+
+
+
+
+//cars.sort()   // Sorts the array
 
 
 function sortaAmigo() {
